2a800ed64b8deac12236e1a5ca4ea7c8469f0880
Javier Ignacio Rivarola <105676056+javotechno@users.noreply.github.com>
2022-12-28 18:47:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+//Función que me aplica el estilo a la opcion seleccionada y quita la previamente seleccionada
+function seleccionar(link) {
+  var opciones = document.querySelectorAll("#links  a");
+  opciones[0].className = "";
+  opciones[1].className = "";
+  opciones[2].className = "";
+  opciones[3].className = "";
+  opciones[4].className = "";
+  link.className = "seleccionado";
+
+  //desaparece el menu una vez que se ha seleccionado una opcion
+  var x = document.getElementById("nav");
+  x.className = "";
+}
+
+//función que muestra el menu responsive. intent 1.
+function responsiveMenu() {
+  var x = document.getElementById("nav");
+  if (x.className === "") {
+    x.className = "responsive";
+  } else {
+    x.className = "";
+  }
+}
+
+//detecto de scrolling
+window.onscroll = function () {
+  efectoHabilidades();
+};
+
+//La animación de la barra de habilidades
+function efectoHabilidades() {
+  var skills = document.getElementById("skills");
+  var distancia_skills =
+    window.innerHeight - skills.getBoundingClientRect().top;
+  if (distancia_skills >= 300) {
+    document.getElementById("html").classList.add("barra-progreso1");
+    document.getElementById("js").classList.add("barra-progreso2");
+    document.getElementById("bd").classList.add("barra-progreso3");
+    document.getElementById("ps").classList.add("barra-progreso4");
+  }
+}
